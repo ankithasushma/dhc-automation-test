267d39be8f85c87f14bf5333c7e22e96792924ce
ankithasushma <ankithasuhsma1997@gmail.com>
2019-08-30 09:37:21 +0530

    third commit and push

--- dhc-automation/dhc-automation/Startup.cs
+++ dhc-automation/dhc-automation/Startup.cs
@@ -25,7 +25,7 @@ namespace dhc_automation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddControllers();
             services.AddConnections();
         }
 
